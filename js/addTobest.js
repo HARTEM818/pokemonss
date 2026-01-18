7daf6d31ccdc565296ccf2edd7d154d3ac1863a8
HARTEM818 <artemhavryliuk777@gmail.com>
2026-01-18 19:21:08 +0200

    rdet

--- js/addTobest.js
+++ js/addTobest.js
@@ -4,25 +4,28 @@ import { getPokemonInfo } from "./getPokemon.js";
 
 export async function addToBest() {
   const data = await getPokemonInfo(refs.inputForSearchPokemons.value);
+  let favorites = JSON.parse(localStorage.getItem("favoritePokemons")) || [];
 
+  refs.addBestBtn.style.display = favorites.includes(data.name)
+    ? "none"
+    : "block";
 
-  refs.addBestBtn.style.display = "block";
-
-  refs.addBestBtn.addEventListener("click", () => {
-    let favorites = JSON.parse(localStorage.getItem("favoritePokemons")) || [];
-
+  refs.addBestBtn.onclick = () => {
     if (!favorites.includes(data.name)) {
+      console.log("idusfb");
+      
       favorites.push(data.name);
       localStorage.setItem("favoritePokemons", JSON.stringify(favorites));
 
       renderFavorite(data.name);
       refs.addBestBtn.style.display = "none";
     }
-  });
+  };
 }
 
 
-export function renderFavorite(name) {
+export async function renderFavorite(name) {
+
   const p = document.createElement("p");
   p.innerText = name;
 
@@ -39,7 +42,6 @@ export function renderFavorite(name) {
     const favorites = JSON.parse(localStorage.getItem("favoritePokemons")) || [];
     const updated = favorites.filter((item) => item !== name);
     localStorage.setItem("favoritePokemons", JSON.stringify(updated));
-
     refs.addBestBtn.style.display = "block";
   });
 }
